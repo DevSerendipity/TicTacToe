32505c990b1381fa4da0381242b2a74d829fb55e
Islam <islamsakrak2002@gmail.com>
2022-01-31 10:47:27 +0100

    Major Improvements in readability
    Improved the spacing , introduced global fields , added an static initializer, removed the magic values, added is and has to conditions, extracted lots of functionality to methods that serve a specific purpose

--- src/main/java/tictactoe/TicTacToe.java
+++ src/main/java/tictactoe/TicTacToe.java
@@ -1,65 +1,97 @@
 package tictactoe;
+
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class TicTacToe {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String[] grid = new String[9];
-        int human;
-        int computer;
-        for (int i = 0; i < 9; i++) {
-            grid[i] = String.valueOf(i + 1);
+
+    private static final String PLAYER_CHARACTER = "X";
+    private static final String COMPUTER_CHARACTER = "O";
+    private static final String[] grid = new String[9];
+
+    static {
+        for (int i = 0; i < grid.length; i++) {
+            TicTacToe.grid[i] = String.valueOf(i + 1);
         }
-        while (!(grid[0].equals("X") && grid[1].equals("X") && grid[2].equals("X") && grid[3].equals("X") && grid[4].equals("X") && grid[5].equals("X") && grid[6].equals("X") && grid[7].equals("X") && grid[8].equals("X"))) {
+    }
+
+    public static void main(String[] args) {
+        gameLoop();
+    }
+
+    private static void gameLoop() {
+        Scanner input = new Scanner(System.in);
+        int player;
+        int computer;
+        while (Arrays.stream(grid).distinct().count() > 2) {
             System.out.println("Pick a number from 1 - 9 ");
-            human = input.nextInt();
+            player = input.nextInt();
             computer = (int) (Math.random() * 9 + 1);
-            while(grid[human-1].equals("X") || grid[human-1].equals("O") ){
+
+            while(hasTiles(grid[player - 1])){
                 System.out.println("Try again that tile is taken");
-                human = input.nextInt();
+                player = input.nextInt();
             }
-            grid[human - 1] = "X";
-            while(grid[computer-1].equals("X") || grid[computer-1].equals("O") || computer == human){
+            grid[player - 1] = PLAYER_CHARACTER;
+
+            while(hasTiles(grid[computer-1]) || computer == player){
                 if(Arrays.stream(grid).distinct().count() == 2){
-                    System.out.println("Unique " + Arrays.stream(grid).distinct().count());
-                    System.out.println(grid[0] + "|" + grid[1] + "|" + grid[2]);
-                    System.out.println("-----");
-                    System.out.println(grid[3] + "|" + grid[4] + "|" + grid[5]);
-                    System.out.println("-----");
-                    System.out.println(grid[6] + "|" + grid[7] + "|" + grid[8]);
+                    gridView();
                     System.exit(200);
                 }
-                System.out.println("Bot will try again that tile is taken");
                 computer = (int) (Math.random() * 9 + 1);
             }
-            grid[computer - 1] = "O";
-            System.out.println(grid[0] + "|" + grid[1] + "|" + grid[2]);
-            System.out.println("-----");
-            System.out.println(grid[3] + "|" + grid[4] + "|" + grid[5]);
-            System.out.println("-----");
-            System.out.println(grid[6] + "|" + grid[7] + "|" + grid[8]);
-            if(grid[0].equals("X") && grid[1].equals("X") && grid[2].equals("X")
-            || grid[3].equals("X") && grid[4].equals("X") && grid[5].equals("X")
-            || grid[6].equals("X") && grid[7].equals("X") && grid[8].equals("X")
-            || grid[0].equals("X") && grid[3].equals("X") && grid[6].equals("X")
-                    || grid[1].equals("X") && grid[4].equals("X") && grid[7].equals("X")
-                    || grid[2].equals("X") && grid[5].equals("X") && grid[8].equals("X")
-            || grid[0].equals("X") && grid[4].equals("X") && grid[8].equals("X")
-                    || grid[2].equals("X") && grid[4].equals("X") && grid[6].equals("X")
-            ){
-                System.out.println("Human Won\nGame Over");
-                System.exit(200);
-            }else if(grid[0].equals("O") && grid[1].equals("O") && grid[2].equals("O")
-                    || grid[3].equals("O") && grid[4].equals("O") && grid[5].equals("O")
-                    || grid[6].equals("O") && grid[7].equals("O") && grid[8].equals("O")
-                    || grid[0].equals("O") && grid[3].equals("O") && grid[6].equals("O")
-                    || grid[1].equals("O") && grid[4].equals("O") && grid[7].equals("O")
-                    || grid[2].equals("O") && grid[5].equals("O") && grid[8].equals("O")
-                    || grid[0].equals("O") && grid[4].equals("O") && grid[8].equals("O")
-                    || grid[2].equals("O") && grid[4].equals("O") && grid[6].equals("O")){
-                System.out.println("Computer Won\nGame Over");
-                System.exit(205);
-            }
+
+            grid[computer - 1] = COMPUTER_CHARACTER;
+            gridView();
+
+            winner();
         }
+    }
+
+    private static boolean hasTiles(String tile) {
+        return tile.equals("X") || tile.equals("O");
+    }
+
+    private static void gridView() {
+        System.out.println(grid[0] + "|" + grid[1] + "|" + grid[2]);
+        System.out.println("-----");
+        System.out.println(grid[3] + "|" + grid[4] + "|" + grid[5]);
+        System.out.println("-----");
+        System.out.println(grid[6] + "|" + grid[7] + "|" + grid[8]);
+    }
+
+    private static void winner() {
+        if(isWinner(PLAYER_CHARACTER)
+        ){
+            System.out.println("Player Won\nGame Over");
+            System.exit(200);
+        }else if(isWinner(COMPUTER_CHARACTER)){
+            System.out.println("Computer Won\nGame Over");
+            System.exit(205);
+        }
+    }
+
+    private static boolean isWinner(String userCharacter) {
+        return hasWonHorizontally(userCharacter)
+                || hasWonVertically(userCharacter)
+                || hasWonDiagonally(userCharacter);
+    }
+
+    private static boolean hasWonDiagonally(String userCharacter) {
+        return grid[0].equals(userCharacter) && grid[4].equals(userCharacter) && grid[8].equals(userCharacter)
+                || grid[2].equals(userCharacter) && grid[4].equals(userCharacter) && grid[6].equals(userCharacter);
+    }
+
+    private static boolean hasWonVertically(String userCharacter) {
+        return grid[0].equals(userCharacter) && grid[3].equals(userCharacter) && grid[6].equals(userCharacter)
+                || grid[1].equals(userCharacter) && grid[4].equals(userCharacter) && grid[7].equals(userCharacter)
+                || grid[2].equals(userCharacter) && grid[5].equals(userCharacter) && grid[8].equals(userCharacter);
+    }
+
+    private static boolean hasWonHorizontally(String userCharacter) {
+        return grid[0].equals(userCharacter) && grid[1].equals(userCharacter) && grid[2].equals(userCharacter)
+                || grid[3].equals(userCharacter) && grid[4].equals(userCharacter) && grid[5].equals(userCharacter)
+                || grid[6].equals(userCharacter) && grid[7].equals(userCharacter) && grid[8].equals(userCharacter);
     }
 }
