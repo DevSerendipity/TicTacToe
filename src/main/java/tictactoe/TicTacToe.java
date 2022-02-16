d3d8836e291ebea06f095e6c44cdfc5cd1135c60
Islam <islamsakrak2002@gmail.com>
2022-02-16 11:27:58 +0100

    Refactoring naming and comments
    Removing comments and changing from tiles to cells

--- src/main/java/tictactoe/TicTacToe.java
+++ src/main/java/tictactoe/TicTacToe.java
@@ -3,50 +3,13 @@ package tictactoe;
 import java.util.*;
 
 // TODO Naming conventions | Work on naming not to name them regarding the topic that they cover, name them regarding what the player can expect them to do
-/*
-IMPROVEMENTS TO BE MADE LATER
 enum Cell {
-  NONE(' '), X('X'), O('O');
-  private final char c;
-  Side(char c){
-    this.c = c;
-  }
-
-  public char getChar(){
-    return c;
-  }
-}
-
-Change your enum Player to this.
-enum State {
-  WINNER_X, WINNER_O, TIE, RUNNING
-}
-record Coord(int row, int col){}
-interface Player {
-  Coord play(Cell[][] board);
-}
-class TicTacToe {
-  private final Player p1, p2;
-  private final Cell[][] board;
-  ...
-  TicTacToe(Player p1, Player p2){
-    this.p1 = p1;
-    this.p2 = p2;
-    this.board = ...;
-  }
-
-  Cell[][] getCurrentBoard(){};
-  boolean playOnce(){};
-  State getState();
-}
- */
-enum Player {
     HUMAN("X"),
     COMPUTER("O");
 
     private final String symbol;
 
-    Player(String symbol) {
+    Cell(String symbol) {
         this.symbol = symbol;
     }
 
@@ -58,12 +21,12 @@ enum Player {
 
 public class TicTacToe {
 
-    private final Player[][] grid = new Player[ROWS][COLS];
+    private final Cell[][] grid = new Cell[ROWS][COLS];
     private final Scanner input = new Scanner(System.in);
     private static final int COLS = 3;
     private static final int ROWS = 3;
 
-    public TicTacToe(){
+    public TicTacToe() {
         displayGrid();
     }
 
@@ -79,12 +42,12 @@ public class TicTacToe {
                 System.out.println("DRAW");
                 return;
             }
-            if(hasWinner()){
+            if (hasWinner()) {
                 displayGrid();
                 break;
             }
             runComputerTurn();
-            if(hasWinner()){
+            if (hasWinner()) {
                 displayGrid();
                 break;
             }
@@ -94,22 +57,22 @@ public class TicTacToe {
     }
 
     private boolean hasWinner() {
-        if (isWinner(Player.HUMAN)) {
+        if (isWinner(Cell.HUMAN)) {
             return true;
-        } else return isWinner(Player.COMPUTER);
+        } else return isWinner(Cell.COMPUTER);
     }
 
     private void runHumansTurn() {
         System.out.println("Pick a number from 1 - 9 ");
         int humanSelectedPosition = input.nextInt();
-        while (isCellTaken(getTilePositioning(humanSelectedPosition)) || isRightInput(humanSelectedPosition)) {
+        while (isCellTaken(getCellPositioning(humanSelectedPosition)) || isRightInput(humanSelectedPosition)) {
             System.out.println("Try again ");
             humanSelectedPosition = input.nextInt();
         }
-        grid[(humanSelectedPosition - 1 )/3][(humanSelectedPosition - 1) % 3] = Player.HUMAN;
+        grid[(humanSelectedPosition - 1) / 3][(humanSelectedPosition - 1) % 3] = Cell.HUMAN;
     }
 
-    private Player getTilePositioning(int humanSelectedPosition) {
+    private Cell getCellPositioning(int humanSelectedPosition) {
         return grid[(humanSelectedPosition - 1) / 3][(humanSelectedPosition - 1) % 3];
     }
 
@@ -118,7 +81,7 @@ public class TicTacToe {
     }
 
     private boolean isDraw() {
-        for (Player[] players : grid) {
+        for (Cell[] players : grid) {
             for (int i = 0; i < grid.length; i++) {
                 if (players[i] == null || players[i].getSymbol().equals(" ")) {
                     return false;
@@ -127,8 +90,9 @@ public class TicTacToe {
         }
         return true;
     }
+
     private void displayGrid() {
-        for (Player[] players : grid) {
+        for (Cell[] players : grid) {
             for (int index = 0; index < grid.length; index++) {
                 String symbolText = players[index] == null ? " " : players[index].getSymbol();
 
@@ -142,64 +106,64 @@ public class TicTacToe {
 
     private void runComputerTurn() {
         int computerSelectedPosition = (int) (Math.random() * 9 + 1);
-        while (isCellTaken(getTilePositioning(computerSelectedPosition))) {
+        while (isCellTaken(getCellPositioning(computerSelectedPosition))) {
             computerSelectedPosition = (int) (Math.random() * 9 + 1);
         }
-        grid[(computerSelectedPosition - 1 )/3][(computerSelectedPosition - 1) % 3] = Player.COMPUTER;
+        grid[(computerSelectedPosition - 1) / 3][(computerSelectedPosition - 1) % 3] = Cell.COMPUTER;
     }
 
     private void getWinner() {
-        if (isWinner(Player.HUMAN)) {
+        if (isWinner(Cell.HUMAN)) {
             System.out.println("Player won");
         } else {
             System.out.println("Computer won");
         }
     }
 
-    private boolean isCellTaken(Player playerTile) {
-        return playerTile != null;
+    private boolean isCellTaken(Cell playerCell) {
+        return playerCell != null;
     }
 
     private boolean isLastRowIndex(int col) {
         return col == 2;
     }
 
-    private boolean isWinner(Player userCharacter) {
+    private boolean isWinner(Cell userCharacter) {
         return hasWonHorizontally(userCharacter)
                 || hasWonVertically(userCharacter)
                 || hasWonDiagonally(userCharacter);
     }
 
-    private boolean hasWonDiagonally(Player userCharacter) {
+    private boolean hasWonDiagonally(Cell userCharacter) {
         return leftToRightDiagonalWin(userCharacter) || rightToLeftDiagonalWin(userCharacter);
     }
 
-    private boolean rightToLeftDiagonalWin(Player userCharacter) {
+    private boolean rightToLeftDiagonalWin(Cell userCharacter) {
         for (int rows = 0; rows < ROWS; rows++) {
-            Player tile = grid[COLS - rows -1] [rows];
-            if (tile != userCharacter) {
+            Cell cell = grid[COLS - rows - 1][rows];
+            if (cell != userCharacter) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean leftToRightDiagonalWin(Player userCharacter) {
+    private boolean leftToRightDiagonalWin(Cell userCharacter) {
         for (int i = 0; i < ROWS; i++) {
-            Player tile = grid[i][i];
-            if (tile != userCharacter) {
+            Cell cell = grid[i][i];
+            if (cell != userCharacter) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean hasWonVertically(Player userCharacter) {
+    private boolean hasWonVertically(Cell userCharacter) {
         for (int col = 0; col < COLS; col++) {
             boolean isColWon = true;
             for (int row = 0; row < ROWS; row++) {
-                Player tileIndex = grid[row] [col];
-                if (tileIndex != userCharacter) {
+                Cell cellIndex = grid[row][col];
+                if (cellIndex != userCharacter) {
                     isColWon = false;
                     break;
                 }
@@ -212,12 +176,12 @@ public class TicTacToe {
         return false;
     }
 
-    private boolean hasWonHorizontally(Player userCharacter) {
+    private boolean hasWonHorizontally(Cell userCharacter) {
         for (int row = 0; row < ROWS; row++) {
             boolean isRowWon = true;
             for (int col = 0; col < COLS; col++) {
-                Player tileIndex = grid[row][col];
-                if (tileIndex != userCharacter) {
+                Cell cellIndex = grid[row][col];
+                if (cellIndex != userCharacter) {
                     isRowWon = false;
                     break;
                 }
